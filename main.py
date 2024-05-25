654c208630bf72b9effd52e613a154b802892469
Paul Byrne <thegoldenaxe111@gmail.com>
2024-05-25 13:07:36 +0200

    Changed the functions to variables that store dictionaries of the letters and gives each letter a number using a for loop.

--- main.py
+++ main.py
@@ -3,30 +3,19 @@ with open("wizard_of_oz.txt", "r", encoding="utf-8") as f: #accesses the text fi
 
 chars = sorted(set(text)) # grabs every instance of a character and then puts it in a string 
 
-def string_to_int_func(): # function to enumerate/tokenize each character from chars
-    string_to_int = {}
-    for i, ch in enumerate(chars):
-        string_to_int[ch] = i
+string_to_int = { ch:i for i,ch in enumerate(chars) } # creates a dict with all the chars saved, sorts a number for each letter with a for loop
+int_to_string = { i:ch for i,ch in enumerate(chars)} # creates a dict with all the chars and nums saved, but reversed, sorts a number for each letter with a for loop
 
-    return string_to_int
+encode = lambda x: [string_to_int[c] for c in x] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
+                                                # Iterates over that using the for loop until the whole string is complete
+                                                # x is the input, c is the individual characters in the input
 
-def int_to_string_func(): # function to return enumerated characters back to regular letters
-    int_to_string = {}
-    for ch, i in enumerate(chars):
-        int_to_string[i] = ch
-
-    return int_to_string
-
-encode = lambda x: [string_to_int_func()[c] for c in x] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
-                                                        # Iterates over that using the for loop until the whole string is complete
-                                                        # x is the input, c is the individual characters in the input
-
-decode = lambda y: ''.join(int_to_string_func()[l] for l in y) # doesn't work atm
+decode = lambda y: ''.join(int_to_string[l] for l in y) # successful
 
 encoded = encode("Hello")
 
-print(string_to_int_func()) # successful
+print(string_to_int) # successful
 
 print(encoded)
 
-print(decode(encoded)) # doesn't work atm
+print(decode(encoded)) # successful
