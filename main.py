a9769f9c023b1c75481ef3cdc40f80b942a39751
Paul Byrne <thegoldenaxe111@gmail.com>
2024-05-29 23:06:52 +0200

    Finally got the torch module to install and working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import torch
+
 with open("wizard_of_oz.txt", "r", encoding="utf-8") as f: #accesses the text file in a read format
     text = f.read() # saves the text in a variable
 
@@ -9,13 +11,14 @@ int_to_string = { i:ch for i,ch in enumerate(chars)} # creates a dict with all t
 encode = lambda x: [string_to_int[c] for c in x] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
                                                 # Iterates over that using the for loop until the whole string is complete
                                                 # x is the input, c is the individual characters in the input
-
 decode = lambda y: ''.join(int_to_string[l] for l in y) # successful
 
-encoded = encode("Hello")
 
-print(string_to_int) # successful
-
-print(encoded)
-
-print(decode(encoded)) # successful
+data = torch.tensor(encode(text), dtype=torch.long)
+#encoded = encode("Hello")
+#
+#print(string_to_int) # successful
+#
+#print(encoded)
+#
+#print(decode(encoded)) # successful
