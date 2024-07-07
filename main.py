d8a6810c06f891b9bfa9a0fec1e296d8e6f5154e
PaulByrne24 <thegoldenaxe111@gmail.com>
2024-07-07 15:14:49 +0200

    Changed the code into a class so I can compartmentalize (organize it better) the code. At the moment I'm planning on writing the code in objects (OPP).

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import torch
+import torch.nn as nn
 
-with open("wizard_of_oz.txt", "r", encoding="utf-8") as f: #accesses the text file in a read format
-    text = f.read() # saves the text in a variable
+class EncoderDecoder:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.text = self.read_file()
+        self.chars = sorted(set(self.text)) # grabs every instance of a character and then puts it in a string
 
-chars = sorted(set(text)) # grabs every instance of a character and then puts it in a string 
+        self.string_to_int = {ch: i for i, ch in enumerate(self.chars)} # creates a dict with all the chars saved, sorts a number for each letter with a for loop
+        self.int_to_string = {i: ch for i, ch in enumerate(self.chars)} # creates a dict with all the chars and nums saved, but reversed, sorts a number for each letter with a for loop
 
-string_to_int = { ch:i for i,ch in enumerate(chars) } # creates a dict with all the chars saved, sorts a number for each letter with a for loop
-int_to_string = { i:ch for i,ch in enumerate(chars)} # creates a dict with all the chars and nums saved, but reversed, sorts a number for each letter with a for loop
+        self.data = torch.tensor(self.encode(self.text), dtype=torch.long)
 
-encode = lambda x: [string_to_int[c] for c in x] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
-                                                # Iterates over that using the for loop until the whole string is complete
-                                                # x is the input, c is the individual characters in the input
-decode = lambda y: ''.join(int_to_string[l] for l in y) # successful
+    def read_file(self):
+        with open(self.file_path, "r", encoding="utf-8") as f: # accesses the text file in a read format
+            return f.read() # saves the text in a variable
 
+    def encode(self, input_text):
+        return [self.string_to_int[c] for c in input_text] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
+                                                        # Iterates over that using the for loop until the whole string is complete
+                                                        # x is the input, c is the individual characters in the input
 
-data = torch.tensor(encode(text), dtype=torch.long)
+    def decode(self, encoded_list):
+        return ''.join(self.int_to_string[l] for l in encoded_list)
 
-print(data[:100])
+    def get_encoded_data(self):
+        return self.data[:100]
+
+file_path = "wizard_of_oz.txt"
+encoder_decoder = EncoderDecoder(file_path) # saves the class into a variable with "file_path" as it's attribute
+print(encoder_decoder.get_encoded_data())
