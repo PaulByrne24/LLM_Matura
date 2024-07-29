8ead0f3c80215cbb94df981489dd20806779b0ae
PaulByrne24 <thegoldenaxe111@gmail.com>
2024-07-29 16:06:23 +0200

    Made the main body of the Transformer. It was significantly easier to make than the Self Attention class, also started to write the embedder mechanism, not sure if I'll change it later and incorporate it to the encoder class. Mainly commiting so transfer the code to another computer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
-import numpy
 
-class textEncoderDecoder:
+class textTokenizer:
     def __init__(self, file_path):
         self.file_path = file_path
         self.text = self.read_file()
@@ -17,19 +16,19 @@ class textEncoderDecoder:
         with open(self.file_path, "r", encoding="utf-8") as f: # accesses the text file in a read format
             return f.read() # saves the text in a variable
 
-    def encode(self, input_text):
+    def tokenize(self, input_text):
         return [self.string_to_int[c] for c in input_text] # Creates a list, uses the string_to_int_func() function to convert the input to it's numerical value. 
                                                         # Iterates over that using the for loop until the whole string is complete
                                                         # x is the input, c is the individual characters in the input
 
-    def decode(self, encoded_list):
+    def detokenize(self, encoded_list):
         return ''.join(self.int_to_string[l] for l in encoded_list)
 
-    def get_encoded_data(self):
+    def get_tokens(self):
         return self.data[:100]
 
-encoder_decoder = textEncoderDecoder("wizard_of_oz.txt") # saves the class into a variable with the wizard of oz text
-print(encoder_decoder.get_encoded_data())
+encoder_decoder = textTokenizer("wizard_of_oz.txt") # saves the class into a variable with the wizard of oz text
+print(encoder_decoder.get_tokens())
 
 class SelfAttention(nn.Module):
     def __init__(self, embed_size, heads):
@@ -72,3 +71,37 @@ class SelfAttention(nn.Module):
         # applies the fc_out linear neural network and uses the arrays of "output" as it's input. Then returns the output of the NN.
         output = self.fc_out(output)
         return output
+    
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+
+        # two layers that normalize the inputs to make it easier and quicker for the transformer to work with the embeds
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size), # expands embedding size in a linear nn
+            nn.ReLU(), # makes the model non-linear by making any negative value 0
+            nn.Linear(forward_expansion*embed_size, embed_size) # reduces embedding size to original
+            )
+
+        self.dropout = nn.Dropout(dropout) # randomly sets neurons/nodes to 0 to make the model find other alternatives that aren't reliant on specific nodes
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+
+        dropped = self.dropout(self.norm1(attention + query)) # drops a random amount of neurons in the first normalization
+        forward = self.feed_forward(dropped) # puts the var dropped in the feed_forward sequence
+        output = self.dropout(self.norm2(forward + dropped)) # drops random neurons of the second normalization 
+
+        return output
+    
+class Embedder(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion, vocab_size, length):
+        super(Embedder, self).__init__()
+        self.embed_size = embed_size
+        self.word_embedding = nn.Embedding(vocab_size, embed_size) # embedds the words with a vector in 3d space
+        self.posit_embedding = nn.Embedding(length, embed_size) # saves the positions of the words in the original sentence
